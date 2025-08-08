880db6f03538217ae4b22262e2e0243966bc2f98
FRQ <frq6692@gmail.com>
2025-08-08 17:26:13 +0500

    48reuf

--- tina/config.ts
+++ tina/config.ts
@@ -6,8 +6,8 @@ const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || 'main'
 
 export default defineConfig({
 	branch,
-	clientId: '26f19c2e-f677-4e78-a063-067027fb4eb2', // Get this from tina.io
-	token: '6bb9420b5ffe59abec9be535fd7f606f52facad5', // Get this from tina.io
+	clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID, // Get this from tina.io
+	token: process.env.TINA_TOKEN, // Get this from tina.io,
 	cmsCallback: (cms) => {
 		cms.flags.set('branch-switcher', true)
 		return cms
@@ -19,7 +19,7 @@ export default defineConfig({
 	media: {
 		tina: {
 			mediaRoot: '/src/assets/images',
-			publicFolder: ''
+			publicFolder: 'public'
 		}
 	},
 	schema: {
